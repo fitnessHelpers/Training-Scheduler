edc78a126b33e92a9d5f0f153f785af9a953a2d6
avoronkov <aleksandrs.voronkovs@ideaportriga.lv>
2019-05-21 18:35:35 +0300

    Development environment setup (babel, webpack). Project first preparations

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1> 🚴 Training scheduler is under development! 🚴 </h1>
+      <p> Coming soon! </p>
+    </div>
+  );
+};
+
+export default App;
